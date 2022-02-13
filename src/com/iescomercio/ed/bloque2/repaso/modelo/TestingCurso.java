cf6278b0fd8084036a98d268bcc03258952b3084
= <=>
2022-02-13 18:40:34 +0100

    He creado los test de Aniadir Alumno y de Esta registrado

--- src/com/iescomercio/ed/bloque2/repaso/modelo/TestingCurso.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/TestingCurso.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.Test;
 public class TestingCurso {
 	private Persona primero = new Persona("12345678M", "Manolito1","Ape1");	
 	private Persona segundo = new Persona("12345679M", "Manolito2","Ape2");
+	private Persona tercero = new Persona("12345674M", "Manolito3","Ape3");
 	private List<Persona> listaAlumnos = new ArrayList<>();
 	private Curso curso1;
-	
 	
 	
 	
@@ -24,5 +24,26 @@ public class TestingCurso {
 		int aux = curso1.size();
 		curso1.eliminarAlumno("12345679M");
 		assertEquals(aux, curso1.size()+1);
+		assertThrows(Exception.class, () -> {curso1.eliminarAlumno("1234569M");});
 	}
+	
+	@Test
+	void TestAniadirAlumno() {
+		listaAlumnos.add(primero);
+		listaAlumnos.add(segundo);
+		curso1 = new Curso(listaAlumnos);
+		int aux = curso1.size();
+		curso1.aniadirAlumno(tercero);
+		assertEquals(aux, curso1.size()-1);
+	}
+	
+	@Test 
+	void TestEstaRegistrado() {
+		listaAlumnos.add(primero);
+		listaAlumnos.add(segundo);
+		curso1 = new Curso(listaAlumnos);
+		assertTrue(curso1.estaRegistrado("12345678M")); 
+		assertFalse(curso1.estaRegistrado("22345678M")); 
+	}
+	
 }
